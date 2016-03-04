f1e8da6d5fcc4f5fe05e0addb9b89e1825adf4d7
Tyler <tkicks32@gmail.com>
2016-03-03 19:50:49 -0500

    quickpush

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	// run when page loads (calls other js functions based on tabs clicked)
+	$('#homeTab').click(function() {
+		console.log("Home tab active");
+	})
+	$('#illusion1Tab').click(function() {
+		SAMTab();
+	})
+	$('#illusion2Tab').click(function() {
+		tTab();
+	})
+	$('#sportsData').click(function() {
+		sportsTab();
+	})
+})
