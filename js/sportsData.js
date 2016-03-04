6e113814011287c0b12df42dc90d73b50868dab7
Tyler <tkicks32@gmail.com>
2016-03-03 20:29:34 -0500

    work

--- js/sportsData.js
+++ js/sportsData.js
@@ -18,13 +18,11 @@ function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
   var table = "<tr><th>Artist</th><th>Title</th></tr>";
-  var x = xmlDoc.getElementsByTagName("CD")
+  var x = xmlDoc.getElementsByTagName("team")
   for (i = 0; i < x.length; i++) { 
     table += "<tr><td>" + 
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
+    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+    "</td><td>";
   }
   document.getElementById("sportsVis").innerHTML = table;
 }
