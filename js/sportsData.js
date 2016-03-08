fa95b3d664fc23069dc05668163196d4d4bcaba5
Tyler <tkicks32@gmail.com>
2016-03-08 00:14:39 -0500

    quickpush gh-pages

--- js/sportsData.js
+++ js/sportsData.js
@@ -7,21 +7,11 @@ function loadXML() {
 	var data = null;
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
-// <<<<<<< HEAD
-  	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-  		var data = getData(xmlhttp);
-      console.log(data[3].getElementsByTagName("height")[0].childNodes[0].nodeValue);
-  	}
-// =======
-// 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-// 			data = getData(xmlhttp);
-// 			console.log(data[3].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-		
-// 			for (i = 0; i < data.length; i++) { 
-// 				console.log(data[i].getElementsByTagName("name")[1].childNodes[0].nodeValue);
-// 			}
-// 		}
-// >>>>>>> f91258b7a259df4604633d43bc0d1cf62c689cd1
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var data = getData(xmlhttp);
+			console.log(data[3].getElementsByTagName("height")[0].childNodes[0].nodeValue);
+			makeSVG(data[0]);
+		}
 	};
 	xmlhttp.open("GET", "./files/sportsData1.xml" , true);
 	xmlhttp.send();
@@ -29,34 +19,19 @@ function loadXML() {
 
 function getData(xml) {
 // <<<<<<< HEAD
-  var i;
-  var xmlDoc = xml.responseXML;
-  // var table = "<tr><th>Team</th><th>Player</th></tr>";
-  var names = xmlDoc.getElementsByTagName("team");
-  for (i = 0; i < names.length; i++) { 
-    names[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-  }
-  // document.getElementById("sportsVis").innerHTML = table;
-  return names;
-// =======
-	// var i;
-	// var xmlDoc = xml.responseXML;
-	// var table = "<tr><th>Team</th><th>Player</th></tr>";
-	// var x = xmlDoc.getElementsByTagName("team")
-	// for (i = 0; i < x.length; i++) { 
-	// 	var playerInfo = x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-	// 	var circle = makeSVG(playerInfo);
-	// 	document.getElementsById('svgCanvas').appendChild(circle);
-	// }
-	// document.getElementById("sportsVis").innerHTML = table;
-	// return x;
+	var i;
+	var xmlDoc = xml.responseXML;
+	var names = xmlDoc.getElementsByTagName("team");
+	for (i = 0; i < names.length; i++) { 
+		names[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+	}
+	return names;
 }
 
-// function makeSVG(playerInfo) {
-// 	var newElt = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-// 	newElt.setAttribute('cx', playerInfo.getElementsByTagName("average")[0].childNodes[0].nodeValue);
-// 	newElt.setAttribute('cy', playerInfo.getElementsByTagName("OBP")[0].childNodes[0].nodeValue);
-// 	newElt.setAttribute('r', playerInfo.getElementsByTagName("weight")[0].childNodes[0].nodeValue);
-// 	return newElt;
-// >>>>>>> f91258b7a259df4604633d43bc0d1cf62c689cd1
-// }
+function makeSVG(playerInfo) {
+	var newElt = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+	newElt.setAttribute('cx', playerInfo.getElementsByTagName("average")[0].childNodes[0].nodeValue);
+	newElt.setAttribute('cy', playerInfo.getElementsByTagName("OBP")[0].childNodes[0].nodeValue);
+	newElt.setAttribute('r', playerInfo.getElementsByTagName("weight")[0].childNodes[0].nodeValue);
+	return newElt;
+}
