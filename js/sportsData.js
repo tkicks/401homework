7368fbe61e2611f7609e019ad1bc1f19e042f804
Tyler <tkicks32@gmail.com>
2016-03-07 23:55:21 -0500

    quickpush

--- js/sportsData.js
+++ js/sportsData.js
@@ -8,14 +8,8 @@ function loadXML() {
 	xmlhttp.onreadystatechange = function() {
   	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
   		var data = getData(xmlhttp);
-      console.log(data[3].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-  	
-      for (i = 0; i < data.length; i++) { 
-        table += "<tr><td>" + 
-        data[i].getElementsByTagName("name")[1].childNodes[0].nodeValue +
-        "</td><td>";
-      }
-    }
+      console.log(data[3].getElementsByTagName("height")[0].childNodes[0].nodeValue);
+  	}
 	};
 	xmlhttp.open("GET", "./files/sportsData1.xml" , true);
 	xmlhttp.send();
@@ -25,10 +19,10 @@ function getData(xml) {
   var i;
   var xmlDoc = xml.responseXML;
   var table = "<tr><th>Team</th><th>Player</th></tr>";
-  var x = xmlDoc.getElementsByTagName("team")
+  var names = xmlDoc.getElementsByTagName("team");
   for (i = 0; i < x.length; i++) { 
     table += "<tr><td>" + 
-    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+    names[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
     "</td><td>";
   }
   document.getElementById("sportsVis").innerHTML = table;
