d409fdb24b25fa10a38a6b76615e06b1f70950f0
Tyler <tkicks32@gmail.com>
2016-03-08 00:10:07 -0500

    quickpush gh-pages

--- js/sportsData.js
+++ js/sportsData.js
@@ -31,12 +31,10 @@ function getData(xml) {
 // <<<<<<< HEAD
   var i;
   var xmlDoc = xml.responseXML;
-  var table = "<tr><th>Team</th><th>Player</th></tr>";
+  // var table = "<tr><th>Team</th><th>Player</th></tr>";
   var names = xmlDoc.getElementsByTagName("team");
   for (i = 0; i < names.length; i++) { 
-    table += "<tr><td>" + 
-    names[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
-    "</td><td>";
+    names[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
   }
   // document.getElementById("sportsVis").innerHTML = table;
   return names;
