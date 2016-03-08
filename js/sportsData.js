33142984f42335e95d8a375eb17975ac6b199b5d
Tyler Kickham <tkicks32@gmail.com>
2016-03-08 10:44:52 -0500

    test

--- js/sportsData.js
+++ js/sportsData.js
@@ -12,7 +12,7 @@ function loadXML() {
 			var data = getData(xmlhttp);
 			console.log(data[3].getElementsByTagName("height")[0].childNodes[0].nodeValue);
 			var circle = makeSVG(data[0]);
-			document.getElementById('svgCanvas').appendChild(circle);
+			document.getElementById('svgCanvas').append(circle);
 		}
 	};
 	xmlhttp.open("GET", "./files/sportsData1.xml" , true);
