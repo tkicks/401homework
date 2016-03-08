d76425dabd99e53fa8045055e1c25caca9a6ae55
Tyler <tkicks32@gmail.com>
2016-03-08 13:21:31 -0500

    quickpush gh-pages

--- js/sportsData.js
+++ js/sportsData.js
@@ -6,14 +6,14 @@ function sportsTab() {
 function loadXML() {
 	var data = null;
 	var xmlhttp = new XMLHttpRequest();
-	d3.select('#svgSpot').append('svg').attr("id", "svgCanvas")
+	var svg = d3.select('#svgSpot').append('svg').attr("id", "svgCanvas")
 		.attr("width", 200)
 		.attr("height", 200);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			var data = getData(xmlhttp);
 			console.log(data[3].getElementsByTagName("height")[0].childNodes[0].nodeValue);
-			var circle = makeSVG(data[0]);
+			var circle = makeSVG(data[0], svg);
 			
 		}
 	};
@@ -31,8 +31,8 @@ function getData(xml) {
 	return names;
 }
 
-function makeSVG(playerInfo) {
-	var circle = d3.select("svgCanvas").append("circle")
+function makeSVG(playerInfo, svg) {
+	var circle = svg.select("svgCanvas").append("circle")
 		.attr("cx", 30)
 		.attr("cy", 30)
 		.attr("r", "10")
